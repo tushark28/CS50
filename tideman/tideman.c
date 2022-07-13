4ae3aca35fc591eca554705299f8531f8427fe97
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-13 11:48:25 +0000

    Wed, Jul 13, 2022, 11:48 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -1,6 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
-
+#include <string.h>
 // Max number of candidates
 #define MAX 9
 
@@ -101,7 +101,7 @@ bool vote(int rank, string name, int ranks[])
 {
     // TODO
     for(int i=0;i<candidate_count;i++){
-        if(strcmp()){
+        if(strcmp(name,candidates[i])==0){
             ranks[rank]= i;
             return true;
         }
