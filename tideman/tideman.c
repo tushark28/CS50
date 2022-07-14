b80ab119231d829b4efd6c10861b67e42b64e77d
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-14 12:58:18 +0000

    Thu, Jul 14, 2022, 12:58 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -172,7 +172,9 @@ void lock_pairs(void)
 {
     // TODO
     for(int i=0;i<candidate_count;i++){
-        
+        for(int j=0;j<candidate_count;j++){
+            
+        }
     }
     return;
 }
