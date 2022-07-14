9b6fb390dbf14705ac027c19c71f08b7c2eb1fd3
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-14 13:36:01 +0000

    Thu, Jul 14, 2022, 1:36 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -177,9 +177,8 @@ void lock_pairs(void)
         temp=0;
         for (int i = 0; i < candidate_count; i++)
         {
-            for (int j = 0; j < candidate_count; j++)
+            for (int j = i+1; j < candidate_count; j++)
             {
-                locked[i][j] = false;
                 if (preferences[i][j] > preferences[j][i])
                 {
                     int diff = preferences[i][j] - preferences[j][i];
