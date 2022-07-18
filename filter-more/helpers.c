62e705f935a32a9cad5d24cd7fe7381c5bbc7dc5
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-18 15:23:47 +0000

    Mon, Jul 18, 2022, 3:23 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,7 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for(int i =0;i<height;i++){
+        for(int j=0;j<width;j++){
+            int avg = round((image[i][j].rgbtRed + image[i][j].rgbtBlue +image[i][j].rgbtGreen)/3);
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtGreen = avg;
+        }
+    }
     return;
 }
 
