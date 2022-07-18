d203d1995c01767acffd094edc052ad3a4a5772c
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-18 15:42:35 +0000

    Mon, Jul 18, 2022, 3:42 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -19,8 +19,8 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for(int i =0;i<height;i++){
         for(int j=0;j<width/2;j++){
-            int tred= image[i][j].rgbt
-            int tgreen=
+            int tred= image[i][j].rgbtRed;
+            int tgreen= image
             int tblue=
 
         }
