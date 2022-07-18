001a3a3bb3eda1ad391153e05ab069eeb947b15e
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-18 16:06:09 +0000

    Mon, Jul 18, 2022, 4:06 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,18 +3,12 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-
-    for (int row = 0; row < height; row++)
-    {
-        for (int col = 0; col < width; col++) // col stands for Column
-        {
-            //Fetch all RGB values and average it
-            float average = round((image[row][col].rgbtRed + image[row][col].rgbtGreen + image[row][col].rgbtBlue) / 3.0);
-
-            //New pixel value
-            image[row][col].rgbtRed = average;
-            image[row][col].rgbtGreen = average;
-            image[row][col].rgbtBlue = average;
+    for(int i =0;i<height;i++){
+        for(int j=0;j<width;j++){
+            int avg = round((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3.0);
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtGreen = avg;
         }
     }
     return;
@@ -28,12 +22,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             int tred= image[i][j].rgbtRed;
             int tgreen= image[i][j].rgbtGreen;
             int tblue= image[i][j].rgbtBlue;
-            image[i][j].rgbtRed = image[i][width - j].rgbtRed;
-            image[i][j].rgbtGreen = image[i][width - j].rgbtGreen;
-            image[i][j].rgbtBlue = image[i][width - j].rgbtBlue;
-            image[i][width - j].rgbtBlue = tblue;
-            image[i][width - j].rgbtGreen = tgreen;
-            image[i][width - j].rgbtRed = tred;
+            
+            image[i][j].rgbtRed = image[i][width -1- j].rgbtRed;
+            image[i][j].rgbtGreen = image[i][width -1- j].rgbtGreen;
+            image[i][j].rgbtBlue = image[i][width -1- j].rgbtBlue;
+
+            image[i][width -1- j].rgbtBlue = tblue;
+            image[i][width -1- j].rgbtGreen = tgreen;
+            image[i][width -1- j].rgbtRed = tred;
         }
     }
     return;
