b0acf57ed3ee102c0f6d5271bcd2d7eadf603c22
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-18 20:01:09 +0000

    Mon, Jul 18, 2022, 8:01 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -1,51 +1,61 @@
 #include <math.h>
 #include "helpers.h"
 // Convert image to grayscale
-int ired(int ci,int arr[],int height){
+int ired(int ci, int arr[], int height)
+{
 
-    if(ci==0){
-        arr[0]= 0;
+    if (ci == 0)
+    {
+        arr[0] = 0;
         arr[1] = 1;
         return 2;
     }
-    else if(ci==height-1){
-        arr[0]=height-1;
-        arr[1]=height-2;
+    else if (ci == height - 1)
+    {
+        arr[0] = height - 1;
+        arr[1] = height - 2;
         return 2;
     }
-    else{
-        arr[0]= ci;
-        arr[1]= ci-1;
-        arr[2]= ci+1;
+    else
+    {
+        arr[0] = ci;
+        arr[1] = ci - 1;
+        arr[2] = ci + 1;
         return 3;
     }
 }
 
-int jred(int cj,int arr2[],int width){
+int jred(int cj, int arr2[], int width)
+{
 
-    if(cj==width-1){
-        arr2[0]=width-1;
-        arr2[1]=width-2;
+    if (cj == width - 1)
+    {
+        arr2[0] = width - 1;
+        arr2[1] = width - 2;
         return 2;
     }
-    else if(cj==0){
-        arr2[0]=0;
-        arr2[1]=1;
+    else if (cj == 0)
+    {
+        arr2[0] = 0;
+        arr2[1] = 1;
         return 2;
     }
-    else{
-        arr2[0]= cj;
-        arr2[1]= cj-1;
-        arr2[2]= cj+1;
+    else
+    {
+        arr2[0] = cj;
+        arr2[1] = cj - 1;
+        arr2[2] = cj + 1;
         return 3;
     }
 }
 
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i =0;i<height;i++){
-        for(int j=0;j<width;j++){
-            int avg = round((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3.0);
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int avg = round((image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen) / 3.0);
             image[i][j].rgbtRed = avg;
             image[i][j].rgbtBlue = avg;
             image[i][j].rgbtGreen = avg;
@@ -57,19 +67,21 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int i =0;i<height;i++){
-        for(int j=0;j<width/2;j++){
-            int tred= image[i][j].rgbtRed;
-            int tgreen= image[i][j].rgbtGreen;
-            int tblue= image[i][j].rgbtBlue;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width / 2; j++)
+        {
+            int tred = image[i][j].rgbtRed;
+            int tgreen = image[i][j].rgbtGreen;
+            int tblue = image[i][j].rgbtBlue;
 
-            image[i][j].rgbtRed = image[i][width -1- j].rgbtRed;
-            image[i][j].rgbtGreen = image[i][width -1- j].rgbtGreen;
-            image[i][j].rgbtBlue = image[i][width -1- j].rgbtBlue;
+            image[i][j].rgbtRed = image[i][width - 1 - j].rgbtRed;
+            image[i][j].rgbtGreen = image[i][width - 1 - j].rgbtGreen;
+            image[i][j].rgbtBlue = image[i][width - 1 - j].rgbtBlue;
 
-            image[i][width -1- j].rgbtBlue = tblue;
-            image[i][width -1- j].rgbtGreen = tgreen;
-            image[i][width -1- j].rgbtRed = tred;
+            image[i][width - 1 - j].rgbtBlue = tblue;
+            image[i][width - 1 - j].rgbtGreen = tgreen;
+            image[i][width - 1 - j].rgbtRed = tred;
         }
     }
     return;
@@ -79,33 +91,39 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE new[height][width];
-    for(int i =0;i<height;i++){
-        for(int j=0;j<width;j++){
-            new[i][j]=image[i][j];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            new[i][j] = image[i][j];
         }
     }
-    for(int i =0;i<height;i++){
-        for(int j=0;j<width;j++){
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             int arr[3];
             int arr2[3];
 
-            int reducedi= ired(i,arr,height);
-            int reducedj= jred(j,arr2,width);
+            int reducedi = ired(i, arr, height);
+            int reducedj = jred(j, arr2, width);
 
-            int sumR=0;
-            int sumG=0;
-            int sumB=0;
+            int sumR = 0;
+            int sumG = 0;
+            int sumB = 0;
 
-            for(int x=0;x<reducedi;x++){
-                for(int y=0;y<reducedj;y++){
+            for (int x = 0; x < reducedi; x++)
+            {
+                for (int y = 0; y < reducedj; y++)
+                {
                     sumR += new[arr[x]][arr2[y]].rgbtRed;
                     sumG += new[arr[x]][arr2[y]].rgbtGreen;
                     sumB += new[arr[x]][arr2[y]].rgbtBlue;
                 }
             }
-            int avgR= round(sumR/(float)(reducedi*reducedj));
-            int avgG= round(sumG/(float)(reducedi*reducedj));
-            int avgB= round(sumB/(float)(reducedi*reducedj));
+            int avgR = round(sumR / (float)(reducedi * reducedj));
+            int avgG = round(sumG / (float)(reducedi * reducedj));
+            int avgB = round(sumB / (float)(reducedi * reducedj));
             image[i][j].rgbtRed = avgR;
             image[i][j].rgbtGreen = avgG;
             image[i][j].rgbtBlue = avgB;
@@ -140,51 +158,60 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     gy[2][2] = 1;
 
     RGBTRIPLE new[height][width];
-    for(int i =0;i<height;i++){
-        for(int j=0;j<width;j++){
-            new[i][j]=image[i][j];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            new[i][j] = image[i][j];
         }
     }
-for(int i =0;i<height;i++){
-        for(int j=0;j<width;j++){
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
             int arr[3];
             int arr2[3];
 
-            int reducedi= ired(i,arr,height);
-            int reducedj= jred(j,arr2,width);
+            int reducedi = ired(i, arr, height);
+            int reducedj = jred(j, arr2, width);
 
-            int sumRx=0;
-            int sumGx=0;
-            int sumBx=0;
+            int sumRx = 0;
+            int sumGx = 0;
+            int sumBx = 0;
 
-            int sumRy=0;
-            int sumGy=0;
-            int sumBy=0;
-            
-            for(int x=0;x<reducedi;x++){
-                for(int y=0;y<reducedj;y++){
-                    for(int t=0;t<3;t++){
-                        for(int m=0;m<3;m++){
-                    sumRx += gx[t][m]*new[arr[x]][arr2[y]].rgbtRed;
-                    sumGx += gx[t][m]*new[arr[x]][arr2[y]].rgbtGreen;
-                    sumBx += gx[t][m]*new[arr[x]][arr2[y]].rgbtBlue;
-                }
+            int sumRy = 0;
+            int sumGy = 0;
+            int sumBy = 0;
+
+            for (int x = 0; x < reducedi; x++)
+            {
+                for (int y = 0; y < reducedj; y++)
+                {
+                    for (int t = 0; t < 3; t++)
+                    {
+                        for (int m = 0; m < 3; m++)
+                        {
+                            sumRx += gx[t][m] * new[arr[x]][arr2[y]].rgbtRed;
+                            sumGx += gx[t][m] * new[arr[x]][arr2[y]].rgbtGreen;
+                            sumBx += gx[t][m] * new[arr[x]][arr2[y]].rgbtBlue;
+                        }
                     }
-                for(int t=0;t<3;t++){
-                        for(int m=0;m<3;m++){
-                    sumRy += gy[t][m]*new[arr[x]][arr2[y]].rgbtRed;
-                    sumGy += gy[t][m]*new[arr[x]][arr2[y]].rgbtGreen;
-                    sumBy += gy[t][m]*new[arr[x]][arr2[y]].rgbtBlue;
-                }
+                    for (int t = 0; t < 3; t++)
+                    {
+                        for (int m = 0; m < 3; m++)
+                        {
+                            sumRy += gy[t][m] * new[arr[x]][arr2[y]].rgbtRed;
+                            sumGy += gy[t][m] * new[arr[x]][arr2[y]].rgbtGreen;
+                            sumBy += gy[t][m] * new[arr[x]][arr2[y]].rgbtBlue;
+                        }
                     }
                 }
             }
-            image[i][j].rgbtRed = sqrt((sumRx*sumRx) + (sumRy*sumRy));
-            image[i][j].rgbtGreen = sqrt((sumGx*sumGx) + (sumGy*sumGy));
-            image[i][j].rgbtBlue = sqrt((sumBx*sumBx) + (sumBy*sumBy));
+            image[i][j].rgbtRed = sqrt((sumRx * sumRx) + (sumRy * sumRy));
+            image[i][j].rgbtGreen = sqrt((sumGx * sumGx) + (sumGy * sumGy));
+            image[i][j].rgbtBlue = sqrt((sumBx * sumBx) + (sumBy * sumBy));
         }
-}
-
+    }
 
     return;
 }
