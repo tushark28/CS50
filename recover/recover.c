d21ad81867473b2c43806eecbccf24ba83f59374
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-19 12:48:18 +0000

    Tue, Jul 19, 2022, 12:48 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef uint8_t BYTE;
+
 int main(int argc, char *argv[])
 {
-    FILE * input = fopen(argv[1],"r");
-    
+    FILE *input = fopen(argv[1], "r");
+
+    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+    {
+        
+    }
+
+    return 0;
 }
