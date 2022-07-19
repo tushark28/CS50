1549cd8e2933e5abe502dbf531a5145730c15a9a
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-19 13:00:20 +0000

    Tue, Jul 19, 2022, 1:00 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,10 +8,15 @@ int main(int argc, char *argv[])
 {
     FILE *input = fopen(argv[1], "r");
     BYTE buffer[BLOCK_SIZE];
+    int jpegflag = 0;
     while (fread(&buffer, 1, BLOCK_SIZE, input) == BLOCK_SIZE)
     {
         if(buffer[0]==0xff && buffer[1]==0xd8 && buffer[2]==0xff && (buffer[3] & 0xf0) == 0xe0){
-            
+            if(jpegflag==0){
+                char filename[8];
+                sprintf(filename,"%03i.jpg",jpegflag);
+                fwrite(&buffer,1,BLOCK_SIZE,)
+            }
         }
     }
 
