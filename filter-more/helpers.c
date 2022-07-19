fb9fc7105a43cc668bbbdb17f940f9f82daac344
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-19 06:13:58 +0000

    Tue, Jul 19, 2022, 6:13 AM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -184,12 +184,10 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             {
                 for (int y = -1; y < 2; y++)
                 {
-                            if(i-x<0 || i+x>=height){
+                            if((i-x<0 || i+x>=height) && (j-y<0 || j+y>=width)){
                                 continue;
                             }
-                            if(j-y<0 || j+y>=width){
-                                continue;
-                            }
+
                             sumRx += gx[x+1][y+1] * new[i+x][y+j].rgbtRed;
                             sumGx += gx[x+1][y+1] * new[x+i][y+j].rgbtGreen;
                             sumBx += gx[x+1][y+1] * new[x+i][y+j].rgbtBlue;
