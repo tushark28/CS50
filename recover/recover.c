8ee4331dae18fecc75e3426d585633c85a3d9540
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-19 13:07:43 +0000

    Tue, Jul 19, 2022, 1:07 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -9,17 +9,21 @@ int main(int argc, char *argv[])
     FILE *input = fopen(argv[1], "r");
     BYTE buffer[BLOCK_SIZE];
     int jpegflag = 0;
+    FILE *output;
     while (fread(&buffer, 1, BLOCK_SIZE, input) == BLOCK_SIZE)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            if (jpegflag == 0)
+            if (jpegflag != 0)
             {
-                char filename[8];
-                sprintf(filename, "%03i.jpg", jpegflag);
-                FILE *output = fopen(filename, "w");
-                fwrite(&buffer, 1, BLOCK_SIZE, output);
+                fclose(output);
             }
+            char filename[8];
+            sprintf(filename, "%03i.jpg", jpegflag);
+            output = fopen(filename, "w");
+            fwrite(&buffer, 1, BLOCK_SIZE, output);
+            jpegflag=1;
+
             else
             {
                 fclose(output);
