0ef7b3e529bd9728a77a24a88390bb7baab32147
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-19 13:03:55 +0000

    Tue, Jul 19, 2022, 1:03 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -11,18 +11,26 @@ int main(int argc, char *argv[])
     int jpegflag = 0;
     while (fread(&buffer, 1, BLOCK_SIZE, input) == BLOCK_SIZE)
     {
-        if(buffer[0]==0xff && buffer[1]==0xd8 && buffer[2]==0xff && (buffer[3] & 0xf0) == 0xe0){
-            if(jpegflag==0){
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            if (jpegflag == 0)
+            {
                 char filename[8];
-                sprintf(filename,"%03i.jpg",jpegflag);
-                FILE* output = fopen(filename,"w");
-                fwrite(&buffer,1,BLOCK_SIZE,output);
+                sprintf(filename, "%03i.jpg", jpegflag);
+                FILE *output = fopen(filename, "w");
+                fwrite(&buffer, 1, BLOCK_SIZE, output);
             }
-            else{
-                
+            else
+            {
+                fclose(output);
+                char filename[8];
+                sprintf(filename, "%03i.jpg", jpegflag);
+                FILE *output = fopen(filename, "w");
+                fwrite(&buffer, 1, BLOCK_SIZE, output);
             }
         }
     }
 
+    fclose(input);
     return 0;
 }
