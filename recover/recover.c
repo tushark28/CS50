e2e0d3fd27e06dc580a3286ea910d624c4347a98
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-19 13:32:57 +0000

    Tue, Jul 19, 2022, 1:32 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,41 +7,51 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
+    //If no argument for forensic image is given as input
     if (argc != 2)
     {
         printf("Usage: ./recover IMAGE");
         return 1;
     }
 
+    //Opening forensic file
     FILE *input = fopen(argv[1], "r");
+
+    //If forensic file is empty
     if (input == NULL)
     {
         printf("Empty forensic image");
         return 1;
     }
 
+    //buffer for each 512 bytes block
     BYTE buffer[BLOCK_SIZE];
-    int jpegflag = 0, jpegcounter = -1;
+    int jpgflag = 0, jpgcounter = -1;
     FILE *output;
+
+    //looping through every 512 bytes till no 512 block is left.
     while (fread(&buffer, 1, BLOCK_SIZE, input) == BLOCK_SIZE)
     {
+        //if the starting 4 bytes is a header of a jpg file
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            if (jpegflag != 0)
+            //if a file is already on write
+            if (jpgflag != 0)
             {
                 fclose(output);
             }
             char filename[8];
-            jpegcounter++;
-            sprintf(filename, "%03i.jpg", jpegcounter);
+            jpgcounter++;
+            sprintf(filename, "%03i.jpg", jpgcounter);
             output = fopen(filename, "w");
             fwrite(&buffer, 1, BLOCK_SIZE, output);
-            jpegflag = 1;
+            jpgflag = 1;
         }
 
         else
         {
-            if (jpegflag != 0)
+            //if a file is already on write
+            if (jpgflag != 0)
             {
                 fwrite(&buffer, 1, BLOCK_SIZE, output);
             }
