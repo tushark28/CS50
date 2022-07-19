36625f443a6dff47affc3e82c71da00314248add
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-19 12:54:50 +0000

    Tue, Jul 19, 2022, 12:54 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,10 +7,12 @@ typedef uint8_t BYTE;
 int main(int argc, char *argv[])
 {
     FILE *input = fopen(argv[1], "r");
-    BYTE buffer[512];
+    BYTE buffer[BLOCK_SIZE];
     while (fread(&buffer, 1, BLOCK_SIZE, input) == BLOCK_SIZE)
     {
-
+        if(buffer[0]==0xff && buffer[1]==0xd8 && buffer[2]==0xff && (buffer[3] & 0xf0) == 0xe0){
+            
+        }
     }
 
     return 0;
