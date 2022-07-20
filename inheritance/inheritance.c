66e58f27ad849ba3f07bfe9d79fe35daf6813017
Tushar Kamboj <iamtktushar@gmail.com>
2022-07-20 08:05:18 +0000

    Wed, Jul 20, 2022, 8:05 AM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -40,7 +40,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person* node = (person*)malloc(sizeof(person));
+    person* new = (person*)malloc(sizeof(person));
     // If there are still generations left to create
     if (generations > 1)
     {
@@ -49,7 +49,8 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        
+        new->parent[0] = *parent0;
+        new->parent[1] = *parent1;
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
     }
