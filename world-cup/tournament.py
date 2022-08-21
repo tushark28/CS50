78c9546338c268dda0649d4ecab4d5ecb3b1ff42
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-21 09:32:29 +0000

    Sun, Aug 21, 2022, 9:32 AM +00:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -31,7 +31,6 @@ def main():
     for teamm in reader:
         counts[teamm['team']]= 0
     simulate_tournament(teams)
-
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
