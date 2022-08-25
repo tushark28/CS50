b695017e46c2c8db355f082e67217788196e6701
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-25 17:37:18 +0000

    Thu, Aug 25, 2022, 5:37 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -7,7 +7,7 @@ user_input = get_string("Text: ")
 l = 0
 w = 1
 s = 0
-# Loop over user_input and get the number of l, w and s in the user_input
+# loop for every character in the string given by the user
 for character in user_input:
     if character.islower() or character.isupper():
         l += 1
@@ -16,11 +16,11 @@ for character in user_input:
     if character == '?' or character == '!' or character == '.':
         s += 1
 
-# Letters and Sentences per 100 w
+# Letters and Sentences per 100 words
 L = l * 100 / w
 S = s * 100 / w
 
-# Coleman-Liau formula
+# Coleman-Liau's formula
 grade = round(0.0588 * L - 0.296 * S - 15.8)
 
 if grade >= 16:
