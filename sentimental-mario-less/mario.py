66a7d26daa712a8b3b609c5f2459b140b389f27d
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-25 09:23:03 +0000

    Thu, Aug 25, 2022, 9:23 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,7 +2,8 @@
 from cs50 import get_int
 while True:
     height = get_int("Height: ")
-    if height>0 
+    if height>0 and height<9:
+        break
 for i in range(height):
     print((height-i-1)*' ',end="")
     print((i+1)*"#")
