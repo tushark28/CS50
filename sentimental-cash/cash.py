0f7d689b4339f0fc1d38706a9cf9e117eca2f9dd
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-25 14:24:56 +0000

    Thu, Aug 25, 2022, 2:24 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -6,10 +6,23 @@ while True:
     change = get_float("Change owed: ")
     if change>=0:
         break
+
 coin = 0
-if (change>=0.25 and change!=0):
+
+if (change >= 0.25 and change != 0):
     coin += change//0.25
     change = change%0.25
 
+if (change >= 0.10 and change != 0):
+    coin += change//0.10
+    change = change%0.10
+
+if (change >= 0.05 and change != 0):
+    coin += change//0.05
+    change = change%0.05
+
+if (change >= 0.01 and change != 0):
+    coin += change//0.01
+    change = change%0.01
 print(coin)
 
