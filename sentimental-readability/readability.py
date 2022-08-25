ce4998eb3f30f0c5697547b5e4134e251c281342
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-25 17:34:30 +0000

    Thu, Aug 25, 2022, 5:34 PM +00:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,21 +4,21 @@ from cs50 import get_string
 # for user input
 user_input = get_string("Text: ")
 
-letters = 0
-words = 1 
-sentences = 0
-# Loop over user_input and get the number of letters, words and sentences in the user_input
-for i in user_input:
-    if i.islower() or i.isupper():
-        letters += 1
-    if i == ' ':
-        words += 1
-    if i == '?' or i == '!' or i == '.':
-        sentences += 1
+l = 0
+w = 1
+s = 0
+# Loop over user_input and get the number of l, w and s in the user_input
+for character in user_input:
+    if character.islower() or character.isupper():
+        l += 1
+    if character == ' ':
+        w += 1
+    if character == '?' or character == '!' or character == '.':
+        s += 1
 
-# Letters and Sentences per 100 words
-L = letters * 100 / words
-S = sentences * 100 / words
+# Letters and Sentences per 100 w
+L = l * 100 / w
+S = s * 100 / w
 
 # Coleman-Liau formula
 grade = round(0.0588 * L - 0.296 * S - 15.8)
