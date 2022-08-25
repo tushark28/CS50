554a91134de00e6c032468e700762acd6f81e998
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-25 14:19:14 +0000

    Thu, Aug 25, 2022, 2:19 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -4,10 +4,11 @@ from cs50 import get_float
 
 while True:
     change = get_float("Change owed: ")
-    coin = 0
-    if (change>=25 and change!=0):
-        coin += change//0.25
-        change = change%0.25
+    if (change<)
+coin = 0
+if (change>=25 and change!=0):
+    coin += change//0.25
+    change = change%0.25
 
-    print(coin)
+print(coin)
 
