6061ea46ab9acdf519840ee0bb109f7c56e70025
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-25 17:02:19 +0000

    Thu, Aug 25, 2022, 5:02 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -11,7 +11,7 @@ coin = 0
 
 if (change >= 0.25 and change != 0):
     coin += change//0.25
-    change = change % 0.25
+    change = round(change % 0.25,2)
 
 if (change >= 0.10 and change != 0):
     coin += change//0.10
