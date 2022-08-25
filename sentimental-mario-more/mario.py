2f67c88e5bc1331ca344e5dc9bef894415787126
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-25 09:52:52 +0000

    Thu, Aug 25, 2022, 9:52 AM +00:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,1 +1,15 @@
 # TODO
+from cs50 import get_int
+
+while True:
+    height = get_int("Height: ")
+    if height > 0 and height < 9:
+        break
+
+for i in range(height):
+    # for spaces.
+    print((height-i-1)*' ', end="")
+    # for "#" symbol
+    print((i+1)*"#")
+    # for two spaces
+    print("  ", end="")
