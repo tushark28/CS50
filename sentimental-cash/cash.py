9128af2859efdb2b64ff139d6a57f7d65a494eb0
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-25 16:59:38 +0000

    Thu, Aug 25, 2022, 4:59 PM +00:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -11,22 +11,19 @@ coin = 0
 
 if (change >= 0.25 and change != 0):
     coin += change//0.25
-    change = change - coin * 0.25
+    change = change % 0.25
 
 if (change >= 0.10 and change != 0):
-    tens = change // 0.10
-    coin += tens
-    change = change - tens * 0.10
+    coin += change//0.10
+    change = change%0.10
 
 if (change >= 0.05 and change != 0):
-    fives = change // 0.05
-    coin += fives
-    change = change - fives * 0.05
+    coin += change//0.05
+    change = change%0.05
 
 if (change >= 0.01 and change != 0):
-    ones = change // 0.01
-    coin += ones
-    change = change - ones * 0.01
+    coin += change//0.01
+    change = change%0.01
 
 print(coin)
 
