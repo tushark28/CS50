70492a9def4183ccea1998d51c6da720eee6938a
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-30 15:05:56 +0000

    Tue, Aug 30, 2022, 3:05 PM +00:00

---
+++ movies/9.sql
@@ -0,0 +1,21 @@
+SELECT
+    count(name)
+FROM
+    people
+WHERE
+    id IN(
+        SELECT
+            person_id
+        FROM
+            stars
+        WHERE
+            movie_id IN(
+                SELECT
+                    id
+                FROM
+                    movies
+                WHERE
+                    year = 2004
+            )
+
+    );
