7064c51a68b4a240fae02c21e53653bbd9e88244
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-30 15:42:27 +0000

    Tue, Aug 30, 2022, 3:42 PM +00:00

--- movies/11.sql
+++ movies/11.sql
@@ -4,5 +4,18 @@ FROM
     movies
 WHERE
     id IN(
-        
+        SELECT
+            movie_id
+        FROM
+            stars
+        WHERE
+            person_id IN(
+                SELECT
+                    id
+                FROM
+                    people
+                WHERE
+                    name = "Chadwick Boseman"
+            )
     )
+LIMIT 5;
