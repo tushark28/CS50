5c2794a304e3cf94692ea059a961eba376e829ea
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-30 16:08:10 +0000

    Tue, Aug 30, 2022, 4:08 PM +00:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,10 +1,35 @@
 select
-    name
+    count(name)
 from
-    people where id in(
-        select person_id from stars where movies_id in(
-            select id from movies where id in(
-                select person_id from stars where person
+    people
+where
+    id in(
+        select
+            person_id
+        from
+            stars
+        where
+            movie_id in(
+                select
+                    id
+                from
+                    movies
+                where
+                    id in(
+                        select
+                            movie_id
+                        from
+                            stars
+                        where
+                            person_id = (
+                                select
+                                    id
+                                from
+                                    people
+                                where
+                                    name = "Kevin Bacon"
+                                    and birth = 1958
+                            )
+                    )
             )
-        )
     )
