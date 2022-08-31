55ffb79363c5b9acd18afbb12ff1e64a0e86f25e
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-31 11:02:38 +0000

    Wed, Aug 31, 2022, 11:02 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -9,3 +9,12 @@ where
     and month = 7
     and day = 28
     and street = "Humphrey Street"
+union all
+select
+    transcript
+from
+    interviews
+where
+    year = 2021
+    and month = 7
+    and day = 28;
