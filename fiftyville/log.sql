b13295a7b5d89727c8e0a2d124a1deb1efffad4c
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-31 10:44:23 +0000

    Wed, Aug 31, 2022, 10:44 AM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,11 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 --for description about the crime.
-SELECT description from crime_scene_reports where year = 2021 and month = 7 and day = 28 and street = "Humphrey Street";
+SELECT
+    description
+from
+    crime_scene_reports
+where
+    year = 2021
+    and month = 7
+    and day = 28
+    and street = "Humphrey Street";
