c3603d4243a924a3f83bfdb508e930ac850505d5
Tushar Kamboj <iamtktushar@gmail.com>
2022-08-31 12:17:05 +0000

    Wed, Aug 31, 2022, 12:17 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -74,3 +74,5 @@ select * from passengers where flight_id = 36;
 select * from airports where id = 4;
 
 select * from phone_calls where caller like "(367)%" and year = 2021 and month = 7 and day = 28;
+
+select * from people where phone_number = "(375) 555-8161";
