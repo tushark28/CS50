6ba9f6aedd2fde428e00cdf9e36daa8a240f33dd
Tushar Kamboj <iamtktushar@gmail.com>
2022-09-26 10:52:19 +0000

    Mon, Sep 26, 2022, 10:52 AM +00:00

--- cs50x/mario-more/mario.c
+++ cs50x/mario-more/mario.c
@@ -1,7 +1,40 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height;
+    do
+    {
+        // Input of height of pyramid
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    //1st loop to print the row
+    for (int i = 0; i < height; i++)
+    {
+        //2nd loop to give space
+        for (int space = 0; space < height - i - 1; space++)
+        {
+            printf(" ");
+        }
+        //3rd loop to print the hash
+        for (int hash = 0; hash < i + 1; hash++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        //4th loop to print hash again after a gap
+        for (int hash = 0; hash < i + 1; hash++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+
+    }
+    return 0;
 }
