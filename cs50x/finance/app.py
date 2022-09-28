14d607df5298f1eba2e7f58b951c6959cb0eaf7e
Tushar Kamboj <iamtktushar@gmail.com>
2022-09-28 17:16:39 +0000

    Wed, Sep 28, 2022, 5:16 PM +00:00

--- cs50x/finance/app.py
+++ cs50x/finance/app.py
@@ -85,7 +85,7 @@ def buy():
             db.execute("UPDATE current_stocks SET stock_count = ?, price = ? WHERE stock_symbol = ? AND user_id = ?", current_stock_count + request.form.get("numberofshares"),current_stock_price + (stock["price"] * request.form.get("numberofshares")), stock["symbol"],session["user_id"])
             db.execute("INSERT INTO stocks_history(stock_name,stock_count,stock_symbol,user_id,time,price,transaction_type) VALUES(?,?,?,?,datetime(now),?,'BOUGHT')", stock["name"], request.form.get("numberofshares"), stock["symbol"], session["user_id"], stock["price"] * request.form.get("numberofshares"))
 
-        elif:
+        else:
             db.execute("INSERT INTO current_stocks(stock_name,stock_count,stock_symbol,user_id,price) VALUES(?,?,?,?,?)", stock["name"],request.form.get("numberofshares"), stock["symbol"],session["user_id"],stock["price"] * request.form.get("numberofshares"))
             db.execute("INSERT INTO stocks_history(stock_name,stock_count,stock_symbol,user_id,time,price,transaction_type) VALUES(?,?,?,?,datetime(now),?,'BOUGHT')", stock["name"], request.form.get("numberofshares"), stock["symbol"], session["user_id"], stock["price"] * request.form.get("numberofshares"))
 
