09ea1c4901c9d53ce5f3c1bfcb982c0742b71359
Tushar Kamboj <iamtktushar@gmail.com>
2022-09-28 06:46:42 +0000

    Wed, Sep 28, 2022, 6:46 AM +00:00

--- cs50x/finance/app.py
+++ cs50x/finance/app.py
@@ -69,8 +69,8 @@ def buy():
         elif stock["price"]*request.form.get("numberofshares") > current_cash:
             return apology("Not Enough Balance", 403)
 
-        db.execute("UPDATE users SET cash = ? WHERE id = ?", current_cash - (stock["price"]*request.form.get("numberofshares")), )
-
+        db.execute("UPDATE users SET cash = ? WHERE id = ?", current_cash - (stock["price"]*request.form.get("numberofshares")),session["user_id"] )
+        db.execute("")
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
