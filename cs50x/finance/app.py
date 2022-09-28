f28a2a58bbf16f62be601251bcfac075b7cc69dc
Tushar Kamboj <iamtktushar@gmail.com>
2022-09-28 13:07:57 +0000

    Wed, Sep 28, 2022, 1:07 PM +00:00

--- cs50x/finance/app.py
+++ cs50x/finance/app.py
@@ -219,17 +219,16 @@ def sell():
         if stock == None:
             return apology("Invalid Stock name", 403)
 
-        db.execute("UPDATE users SET cash = ? WHERE id = ?", current_cash + (stock["price"]*request.form.get("numberofshares")),session["user_id"])
         current_stock_existence = db.execute("SELECT count(*) FROM current_stocks WHERE stock_symbol = ? AND user_id = ?", stock["symbol"], session["user_id"])
         if current_stock_existence != 0:
+        db.execute("UPDATE users SET cash = ? WHERE id = ?", current_cash + (stock["price"]*request.form.get("numberofshares")),session["user_id"])
             current_stock_count = db.execute("SELECT stock_count FROM current_stocks WHERE stock_symbol = ? AND user_id = ?", stock["symbol"], session["user_id"])
             current_stock_price = db.execute("SELECT price FROM current_stocks WHERE stock_symbol = ? AND user_id = ?", stock["symbol"], session["user_id"])
             db.execute("INSERT INTO current_stocks(stock_name,stock_count,stock_symbol,user_id,price) VALUES(?,?,?,?,?)", stock["name"],current_stock_count + request.form.get("numberofshares"), stock["symbol"],session["user_id"],current_stock_price + (stock["price"] * request.form.get("numberofshares")))
             db.execute("INSERT INTO stocks_history(stock_name,stock_count,stock_symbol,user_id,time,price) VALUES(?,?,?,?,datetime(now),?)", stock["name"], request.form.get("numberofshares"), stock["symbol"], session["user_id"], stock["price"] * request.form.get("numberofshares"))
 
         elif:
-            db.execute("INSERT INTO current_stocks(stock_name,stock_count,stock_symbol,user_id,price) VALUES(?,?,?,?,?)", stock["name"],request.form.get("numberofshares"), stock["symbol"],session["user_id"],stock["price"] * request.form.get("numberofshares"))
-            db.execute("INSERT INTO stocks_history(stock_name,stock_count,stock_symbol,user_id,time,price) VALUES(?,?,?,?,datetime(now),?)", stock["name"], request.form.get("numberofshares"), stock["symbol"], session["user_id"],stock["price"] * request.form.get("numberofshares"))
+            return apology("You do not have this share in your portfolio",403)
 
         return redirect("/")
 
