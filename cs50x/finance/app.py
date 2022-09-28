3b36ee71f5450778dc8df4426207b5a9eadfcc10
Tushar Kamboj <iamtktushar@gmail.com>
2022-09-28 13:06:00 +0000

    Wed, Sep 28, 2022, 1:06 PM +00:00

--- cs50x/finance/app.py
+++ cs50x/finance/app.py
@@ -219,7 +219,7 @@ def sell():
         if stock == None:
             return apology("Invalid Stock name", 403)
 
-        db.execute("UPDATE users SET cash = ? WHERE id = ?", current_cash - (stock["price"]*request.form.get("numberofshares")),session["user_id"])
+        db.execute("UPDATE users SET cash = ? WHERE id = ?", current_cash + (stock["price"]*request.form.get("numberofshares")),session["user_id"])
         current_stock_existence = db.execute("SELECT count(*) FROM current_stocks WHERE stock_symbol = ? AND user_id = ?", stock["symbol"], session["user_id"])
         if current_stock_existence != 0:
             current_stock_count = db.execute("SELECT stock_count FROM current_stocks WHERE stock_symbol = ? AND user_id = ?", stock["symbol"], session["user_id"])
