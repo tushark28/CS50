569f9e57440bbaf675c107c0f86adbda6eb6dfc2
Tushar Kamboj <iamtktushar@gmail.com>
2022-09-28 10:51:37 +0000

    Wed, Sep 28, 2022, 10:51 AM +00:00

--- cs50x/finance/app.py
+++ cs50x/finance/app.py
@@ -126,7 +126,7 @@ def login():
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html")
+        return render_template("buy.html")
 
 
 @app.route("/logout")
