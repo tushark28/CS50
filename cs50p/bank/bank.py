d5a772273e6aee62741fa0d88464cb6f310e0878
Tushar Kamboj <iamtktushar@gmail.com>
2023-01-31 18:26:51 +0000

    Tue, Jan 31, 2023, 11:56 PM +05:30

--- cs50p/bank/bank.py
+++ cs50p/bank/bank.py
@@ -2,8 +2,8 @@ greet = input("Greeting: ").strip(" ").lower()
 array = greet.split(" ")
 first = array[0]
 if "hello" == first :
-    print("$0")
+    print("$0",end="")
 elif first[0] == 'h':
-    print("$20")
+    print("$20",end="")
 else:
-    print("$100")
+    print("$100",end="")
