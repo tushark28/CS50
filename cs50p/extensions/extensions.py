cf86db9c0d271c28b708bb747091b16e999a31a6
Tushar Kamboj <iamtktushar@gmail.com>
2023-01-31 20:00:36 +0000

    Wed, Feb 1, 2023, 1:30 AM +05:30

--- cs50p/extensions/extensions.py
+++ cs50p/extensions/extensions.py
@@ -1,2 +1,15 @@
 format = input("File name:").lower()
 format = format.split('.')[1]
+
+if format == 'gif':
+    print("image/gif")
+elif format == 'jpg' or format == 'jpeg':
+    print('image/jpeg')
+elif format == 'zip':
+    print('application/zip')
+elif format == 'pdf':
+    print('application/pdf')
+elif format == 'png':
+    print("image/png")
+elif format == 'text':
+    print(")
