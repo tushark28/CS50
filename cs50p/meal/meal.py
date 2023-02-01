b3219ffab564919d7668cb1e7e19a0597a2b7db9
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-01 20:02:26 +0000

    Thu, Feb 2, 2023, 1:32 AM +05:30

--- cs50p/meal/meal.py
+++ cs50p/meal/meal.py
@@ -1,13 +1,20 @@
 def main():
     time = input("What time is it? ")
-    convert(time)
+    time = convert(time)
+
+    if 7.0 <= time <= 8.0:
+        print("breakfast time")
+    elif 12.0 <= time <= 13.0:
+        print("lunch time")
+    elif 18.0 <= time <= 19.0:
+        print("dinner time")
 
 
 def convert(time):
     x,y = time.split(":")
     x = float(x)
     y = float(y)/60.0
-    time = float(x + y)
+    time = round(float(x + y),1)
     return time
 
 
