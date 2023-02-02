66df7a191a0e98de04e691f71799604722f96b16
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-02 14:15:18 +0000

    Thu, Feb 2, 2023, 7:45 PM +05:30

--- cs50p/coke/coke.py
+++ cs50p/coke/coke.py
@@ -2,6 +2,7 @@ i = 0
 while i<50:
     print(f"Amount Due: {50-i}")
     new = int(input("Insert coin: "))
-    i += new
+    if new == 25 or new == 10 or new == 5:
+        i += new
 
 print(f"Change Owed: {i-50}")
