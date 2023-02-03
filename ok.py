f43eab2068c1e9e414dd8cf639ded6fce7ced435
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-03 19:05:33 +0000

    Sat, Feb 4, 2023, 12:35 AM +05:30

--- ok.py
+++ ok.py
@@ -1,32 +1,32 @@
 import math
 def main():
     self=False
-    num=5
+    num=17
     self = judgeSquareSum(self,num)
 
 def primeFactors(n,root):
-        c = 2
-        flag = 0
-        while(n > 1):
-            if(n % c == 0):
-                if flag==0:
-                    root[n] = 1
-                    flag=1
-                else:
-                    root[n] +=1
-                n = n / c
+    c = 2
+    flag = 0
+    while(n > 1):
+        if(n % c == 0):
+            if flag==0:
+                root[n] = 1
+                flag=1
             else:
-                c = c + 1
-                flag=0
-        return root
+                root[n] +=1
+            n = n / c
+        else:
+            c = c + 1
+            flag=0
+    return root
 
-    def judgeSquareSum(self, c: int) -> bool:
-        if math.sqrt(c).is_integer() or math.sqrt(c/2).is_integer():
-            return True
-        root = {}
-        root = primeFactors(c,root)
-        print(root)
-        return False
+def judgeSquareSum(self, c: int) -> bool:
+    if math.sqrt(c).is_integer() or math.sqrt(c/2).is_integer():
+        return True
+    root = {}
+    root = primeFactors(c,root)
+    print(root)
+    return False
 
 
 
