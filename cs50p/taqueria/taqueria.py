9b01c60fa9d9fb6d747683c4585a2d5a0346ea1a
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-03 15:58:28 +0000

    Fri, Feb 3, 2023, 9:28 PM +05:30

---
+++ cs50p/taqueria/taqueria.py
@@ -0,0 +1,18 @@
+menu =
+{
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+total = 0
+while True:
+    string = input("Item: ").lower().title()
+    try:
+        total += menu[string]
+        print(f"Total: {total}")
