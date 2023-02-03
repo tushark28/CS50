42f4788bb6d823a01204c488420fb1a50dae4632
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-03 10:22:51 +0000

    Fri, Feb 3, 2023, 3:52 PM +05:30

---
+++ cs50p/plates.py
@@ -0,0 +1,15 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+
+    for i in s:
+        if i
+
+
+main()
