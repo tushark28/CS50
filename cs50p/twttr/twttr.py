0bee9672d504b80ad5d2f4ff169a9942209a7c34
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-03 09:20:14 +0000

    Fri, Feb 3, 2023, 2:50 PM +05:30

--- cs50p/twttr/twttr.py
+++ cs50p/twttr/twttr.py
@@ -1,4 +1,8 @@
 string = input("Input: ")
-
+print("Output: ",end="")
 for s in string:
-    if s.lower
+    if s.lower() == 'a' or s.lower() == 'e' or s.lower() == 'i' or s.lower() == 'o' or s.lower() == 'u':
+        continue
+    else:
+        print(s,end="")
+print()
