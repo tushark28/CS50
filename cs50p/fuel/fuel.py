db6336604a1ffb5cc953cb64f47803b90450f107
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-03 14:29:23 +0000

    Fri, Feb 3, 2023, 7:59 PM +05:30

--- cs50p/fuel/fuel.py
+++ cs50p/fuel/fuel.py
@@ -6,5 +6,19 @@ while True:
         y = int(y)
         if x>y:
             continue
-        
+        if y==0:
+            continue
+    except ValueError:
+        pass
+    else:
+        break
+
+per = round(x/y *100)
+
+if per<=1:
+    print("E")
+elif per>=99:
+    print("F")
+else:
+    print(f"{per}%")
 
