4caf5b3ae276e4e97fd51425412d6d8dde9b91b5
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-05 11:55:35 +0000

    Sun, Feb 5, 2023, 5:25 PM +05:30

--- cs50p/grocery/grocery.py
+++ cs50p/grocery/grocery.py
@@ -1,7 +1,11 @@
 items = {}
 while True:
     try:
+        item = input()
         try:
-            item = input()
             items[item] +=1
         except KeyError:
+            items[item] = 1
+    except EOFError:
+        for item in items:
+            print(f"{items[item]} {item.upper()}")
