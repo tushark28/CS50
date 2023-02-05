ff89701eecce56786cf2abfe97aab181d984c2ca
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-05 12:58:14 +0000

    Sun, Feb 5, 2023, 6:28 PM +05:30

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -18,7 +18,8 @@ while True:
         x,y,z = date.split("/")
         if len(z) != 4 or int(x) > 31 or int(y) > 12:
             continue
-        
+
         print(f"{int(z)}-{int(y):02d}-{int(x):02d}")
     except ValueError:
-        pass
+        try:
+            
