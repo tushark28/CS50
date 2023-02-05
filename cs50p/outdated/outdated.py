c3401f85582da6307baf57612279e5e7dd559a36
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-05 14:54:56 +0000

    Sun, Feb 5, 2023, 8:24 PM +05:30

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -1,4 +1,4 @@
-months=[
+months = [
     "January",
     "February",
     "March",
@@ -15,19 +15,21 @@ months=[
 while True:
     date = input("Date: ")
     try:
-        y,x,z = date.split("/")
-        if len(z) != 4 or int(x) > 31 or int(y) > 12:
+        y, x, z = date.split("/")
+        x = int(x)
+        y = int(y)
+        if len(z) != 4 or x > 31 or int(y) > 12:
             continue
 
-        print(f"{int(z)}-{int(y):02d}-{int(x):02d}")
+        print(f"{int(z)}-{y:02d}-{x:02d}")
         break
 
     except ValueError:
-            x,y,z = date.split(" ")
-            if y[-1]!=',' or (x.lower().title() not in months) or len(z)!=4:
-                continue
-            if int(y[:-1])>31:
-                continue
-            print(f"{int(z)}-{(months.index(x.lower().title()) + 1):02d}-{int(y[:-1]):02d}")
-            break
-
+        x, y, z = date.split(" ")
+        if y[-1] != ',' or (x.lower().title() not in months) or len(z) != 4:
+            continue
+        if int(y[:-1]) > 31:
+            continue
+        print(
+            f"{int(z)}-{(months.index(x.lower().title()) + 1):02d}-{int(y[:-1]):02d}")
+        break
