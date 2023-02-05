e2b7fdee91d85d5bb721904ea9c22fece3362fc8
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-05 12:21:45 +0000

    Sun, Feb 5, 2023, 5:51 PM +05:30

---
+++ cs50p/outdated/outdated.py
@@ -0,0 +1,18 @@
+months=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+try:
+    date = input("Date: ")
+    x,y,z = date.split("/")
+    print("")
