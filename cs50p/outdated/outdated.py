a277aa470ec720355b3eea77b1534d42091773a2
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-05 12:58:57 +0000

    Sun, Feb 5, 2023, 6:28 PM +05:30

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -15,11 +15,12 @@ months=[
 while True:
     try:
         date = input("Date: ")
-        x,y,z = date.split("/")
-        if len(z) != 4 or int(x) > 31 or int(y) > 12:
-            continue
+        try:
+            x,y,z = date.split("/")
+            if len(z) != 4 or int(x) > 31 or int(y) > 12:
+                continue
 
-        print(f"{int(z)}-{int(y):02d}-{int(x):02d}")
+            print(f"{int(z)}-{int(y):02d}-{int(x):02d}")
     except ValueError:
         try:
-            
+
