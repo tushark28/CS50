bd8cfe7200d7a146114a91e0ae2d70fa66f8bf7b
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-05 14:45:53 +0000

    Sun, Feb 5, 2023, 8:15 PM +05:30

--- cs50p/outdated/outdated.py
+++ cs50p/outdated/outdated.py
@@ -22,9 +22,9 @@ while True:
         print(f"{int(z)}-{int(y):02d}-{int(x):02d}")
     except ValueError:
             x,y,z = date.split(" ")
-            if y[-1]!=',' or (x not in months) or len(z)!=4:
+            if y[-1]!=',' or (x.lower().title() not in months) or len(z)!=4:
                 continue
             if int(y[:-1])>31:
                 continue
-            print(f"{int(z)}-{(months.index(x) + 1):02d}-{int(y[-1]):02d}")
+            print(f"{int(z)}-{(months.index(x.lower().title()) + 1):02d}-{int(y[:-1]):02d}")
 
