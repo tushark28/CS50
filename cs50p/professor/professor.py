0113d7b34f89bfec95a45e14da59852d9ac2f252
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 16:35:22 +0000

    Mon, Feb 6, 2023, 10:05 PM +05:30

--- cs50p/professor/professor.py
+++ cs50p/professor/professor.py
@@ -5,11 +5,15 @@ def main():
     level = get_level()
     count = 0
     for i in range(10):
-        for j in range(3):
             int1 = generate_integer(level)
             int2 = generate_integer(level)
+        for j in range(3):
             try:
-                
+                ans = input(f"{int1} + {int2} = ")
+            except ValueError:
+                continue
+            if ans == int1 + int2:
+                count = 0
 
 
 def get_level():
