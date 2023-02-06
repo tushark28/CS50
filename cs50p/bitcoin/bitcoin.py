39466d14fec9f3672bfd1043a19ece1412a7315c
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 18:13:24 +0000

    Mon, Feb 6, 2023, 11:43 PM +05:30

--- cs50p/bitcoin/bitcoin.py
+++ cs50p/bitcoin/bitcoin.py
@@ -1,3 +1,10 @@
 import requests, sys
 
-if sys.argv
+if len(sys.argv) != 2:
+    sys.exit("Missing command-line argument ")
+
+try:
+    coin = int(sys.argv[1])
+except:
+    sys.exit("Command-line argument is not a number")
+    
