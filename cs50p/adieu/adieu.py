5349c6541c0a3f1a588803b7990e60a9ffdcf9e5
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 11:12:24 +0000

    Mon, Feb 6, 2023, 4:42 PM +05:30

--- cs50p/adieu/adieu.py
+++ cs50p/adieu/adieu.py
@@ -10,7 +10,8 @@ while True:
         if len(List) == 1:
             new += f"{List[0]}"
         elif len(List) == 2:
-            new[-2]
+            new.replace(',','',3)
+            new+= f"and {List[len(List)-1]}"
         else:
             new+= f"and {List[len(List)-1]}"
         print(new)
