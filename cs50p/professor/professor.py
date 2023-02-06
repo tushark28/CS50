c3e6c7fa13a7fb37492d7d94b29d7e655f350d44
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 16:28:40 +0000

    Mon, Feb 6, 2023, 9:58 PM +05:30

--- cs50p/professor/professor.py
+++ cs50p/professor/professor.py
@@ -8,6 +8,8 @@ level = get_level()
 def get_level():
     try:
         level = int(input("Level: "))
+        if not 1<=level <=3:
+            return get_level()
     except ValueError:
         return get_level()
 
