3d04a7984d174c5235557b3ddc2c4c45a6cecfc5
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 10:35:07 +0000

    Mon, Feb 6, 2023, 4:05 PM +05:30

--- cs50p/adieu/adieu.py
+++ cs50p/adieu/adieu.py
@@ -7,5 +7,6 @@ while True:
         new="Adieu, adieu, to "
         for i in range(len(List)-1):
             new += f"{List[i]}, "
-        new+= f" and {List[len(List)-1]}"
+        new+= f"and {List[len(List)-1]}"
+        print(new)
         break
