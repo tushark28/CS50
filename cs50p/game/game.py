cb0add3cc8cc5e52ddf73d8058bfc735b1c84350
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 11:52:08 +0000

    Mon, Feb 6, 2023, 5:22 PM +05:30

--- cs50p/game/game.py
+++ cs50p/game/game.py
@@ -4,4 +4,19 @@ while True:
     if not level.isdigit():
         continue
     else:
-        level = random.getint()
+        level = random.randint(1,level)
+        break
+while True:
+    guess = input("Guess: ")
+    if not guess.isdigit():
+        continue
+    else:
+        if level>guess:
+            print("Too small!")
+            continue
+        elif level<guess:
+            print("Too large!")
+            continue
+        else:
+            print("Just right!")
+            break
