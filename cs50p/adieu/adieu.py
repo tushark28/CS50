ca0f7a82893511f8921d35cd69ee8a6a1d5381ac
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 10:32:06 +0000

    Mon, Feb 6, 2023, 4:02 PM +05:30

--- cs50p/adieu/adieu.py
+++ cs50p/adieu/adieu.py
@@ -1,7 +1,11 @@
 while True:
+    List = []
     try:
-        string = {}
-        string.append(input("Name: "))
+        List.append(input("Name: "))
 
     except EOFError:
+        new="Adieu, adieu, to "
+        for i in range(len(List)-1):
+            new += f"{List[i]}, "
+        new+= f" and {List[len(List)-1]}"
         break
