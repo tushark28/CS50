df2565350d8ab45da842d671a810f1a98fddda22
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 16:41:23 +0000

    Mon, Feb 6, 2023, 10:11 PM +05:30

--- cs50p/professor/professor.py
+++ cs50p/professor/professor.py
@@ -5,15 +5,24 @@ def main():
     level = get_level()
     count = 0
     for i in range(10):
-            int1 = generate_integer(level)
-            int2 = generate_integer(level)
+        flag = 0
+        int1 = generate_integer(level)
+        int2 = generate_integer(level)
         for j in range(3):
             try:
                 ans = input(f"{int1} + {int2} = ")
             except ValueError:
+                print("EEE")
                 continue
             if ans == int1 + int2:
-                count = 0
+                count += 1
+                break
+            else:
+                print("EEE")
+                flag +=1
+
+        if flag == 3:
+            print(f"{int1} + {int2} = {int1 + int2}")
 
 
 def get_level():
@@ -29,7 +38,8 @@ def get_level():
 
 
 def generate_integer(level):
-    pass
+    if level == 1:
+        random.randint()
 
 
 if __name__ == "__main__":
