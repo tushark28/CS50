7983ea95393e2136a16fdc680e243edbf074f213
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 18:16:40 +0000

    Mon, Feb 6, 2023, 11:46 PM +05:30

--- cs50p/bitcoin/bitcoin.py
+++ cs50p/bitcoin/bitcoin.py
@@ -11,4 +11,6 @@ except ValueError:
 try:
     response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
     print(response.json())
+except requests.RequestException:
+    pass
 
