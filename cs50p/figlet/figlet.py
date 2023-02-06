5bd6300b8419da0f7ff61da90b94e5277b1c70d9
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 10:00:12 +0000

    Mon, Feb 6, 2023, 3:30 PM +05:30

--- cs50p/figlet/figlet.py
+++ cs50p/figlet/figlet.py
@@ -5,7 +5,12 @@ if len(sys.argv) == 3:
     if sys.argv[1] == '-f' or sys.argv[1] == '--font':
         user = input("Input: ")
         print("Output: ")
-        f=pyfiglet.figlet()
+        f=pyfiglet.Figlet(font=sys.argv[2])
         print(f.renderText(user))
     else:
         sys.exit()
+else:
+    user = input("Input: ")
+    print("Output: ")
+    f=pyfiglet.Figlet()
+    print(f.renderText(user))
