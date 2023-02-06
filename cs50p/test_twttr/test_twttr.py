2b1c1c25eaee2011fa4d513adcde4233981b9129
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 19:55:08 +0000

    Tue, Feb 7, 2023, 1:25 AM +05:30

--- cs50p/test_twttr/test_twttr.py
+++ cs50p/test_twttr/test_twttr.py
@@ -2,8 +2,18 @@ from twttr import shorten
 def main():
     test_twttr_lower()
     test_twttr_upper()
-def test_twttr():
+    test_twttr_punctuation()
+    test_twttr_number()
+
+def test_twttr_lower():
     assert shorten("twitter") == "twttr"
+def test_twttr_upper():
+    assert shorten("TWITTER") == "TWTTR"
+def test_twttr_punctuation():
+    assert shorten("tushar, is") == "tshr, s"
+def test_twttr_number():
+    assert shorten("tushar 123") == "tshr 123"
+
 
 if __name__ == "__main__":
     main()
