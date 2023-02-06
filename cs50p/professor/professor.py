0ef1fa216a26d0f9ac9c80677b0759dc82ecdd60
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-06 16:26:26 +0000

    Mon, Feb 6, 2023, 9:56 PM +05:30

---
+++ cs50p/professor/professor.py
@@ -0,0 +1,21 @@
+import random
+
+
+def main():
+level = get_level()
+
+
+def get_level():
+    try:
+        level = int(input("Level: "))
+    except ValueError:
+        return get_level()
+
+
+
+def generate_integer(level):
+
+
+
+if __name__ == "__main__":
+    main()
