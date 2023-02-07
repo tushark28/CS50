89bf8892673486484044eaf44f400506d43c1b5c
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-07 13:09:43 +0000

    Tue, Feb 7, 2023, 6:39 PM +05:30

--- cs50p/test_fuel/test_fuel.py
+++ cs50p/test_fuel/test_fuel.py
@@ -1,5 +1,11 @@
 from fuel import gauge,convert
 import pytest
 def main():
-    test_zero()
-    test_big()
+    test_convert_zero()
+    test_convert_big()
+    test_gauge_99()
+    test_gauge_1()
+    test_gauge_normal()
+
+def test_convert_zero():
+    assert convert()
