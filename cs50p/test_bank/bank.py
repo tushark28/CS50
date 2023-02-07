ae924dda49bca09429ccdcf0b38076c43e5f9a04
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-07 07:16:06 +0000

    Tue, Feb 7, 2023, 12:46 PM +05:30

--- cs50p/test_bank/bank.py
+++ cs50p/test_bank/bank.py
@@ -1,18 +1,18 @@
 def main():
     greet = input("Greeting: ")
     greet = value(greet)
-    print(greet)
+    print("$",greet,sep="")
 
 
 def value(greet):
     array = greet.strip(" ").lower().split(" ")
     first = array[0]
     if first.find("hello") !=-1 :
-        return "$0"
+        return 0
     elif first[0] == 'h':
-        return "$20"
+        return 20
     else:
-        return "$100"
+        return 100
 
 if __name__ == "__main__":
     main()
--- cs50p/test_bank/test_bank.py
+++ cs50p/test_bank/test_bank.py
@@ -7,13 +7,13 @@ def main():
     test_insens()
 
 def test_20():
-    assert value("hey") == "$20"
+    assert value("hey") == 20
 def test_full():
-    assert value("what's up") == "$100"
+    assert value("what's up") == 100
 def test_0():
-    assert value("HELLO") == "$0"
+    assert value("HELLO") == 0
 def test_insens():
-    assert value("hElLo") == "$0"
+    assert value("hElLo") == 0
 
 if __name__ == "__main__":
     main()
