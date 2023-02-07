0909e696057b0cdbf2e7315a092ec2f176f741ba
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-07 13:15:36 +0000

    Tue, Feb 7, 2023, 6:45 PM +05:30

--- cs50p/test_fuel/fuel.py
+++ cs50p/test_fuel/fuel.py
@@ -8,6 +8,8 @@ def convert(fuel):
     x,y = fuel.split("/")
     x = int(x)
     y = int(y)
+    if y==0:
+        raise ZeroDivisionError
     if x>y:
         raise ValueError
     return round(x/y *100)
