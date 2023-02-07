3ea6b33d1d38898010676f904e79385cc1648d47
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-07 07:10:08 +0000

    Tue, Feb 7, 2023, 12:40 PM +05:30

--- cs50p/test_bank/test_bank.py
+++ cs50p/test_bank/test_bank.py
@@ -1,12 +1,19 @@
 from bank import value
 
 def main():
-    test()
+    test_20()
+    test_full()
+    test_0()
+    test_insens()
 
-def test():
+def test_20():
     assert value("hey") == "$20"
+def test_full():
     assert value("what's up") == "$100"
+def test_0():
     assert value("HELLO") == "$0"
+def test_insens():
+    assert value("hElLo") == "$0"
 
 if __name__ == "__main__":
     main()
