12924f1878985bef00b16b5366dd92f76cb62931
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-07 06:48:57 +0000

    Tue, Feb 7, 2023, 12:18 PM +05:30

---
+++ cs50p/test_bank/bank.py
@@ -0,0 +1,18 @@
+def main():
+    greet = input("Greeting: ").strip(" ").lower()
+    greet = value(greet)
+    print(greet)
+
+
+def value(greet):
+    array = greet.split(" ")
+    first = array[0]
+    if first.find("hello") !=-1 :
+        return "$0"
+    elif first[0] == 'h':
+        return "$20"
+    else:
+        return "$100"
+
+if __name__ == "__main__":
+    main()
