4ec4a72632620f90c897679b0149f4d2972b44c5
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-12 16:59:34 +0000

    Sun, Feb 12, 2023, 10:29 PM +05:30

--- cs50p/lines/lines.py
+++ cs50p/lines/lines.py
@@ -1,14 +1,14 @@
 import sys
 
-if len(sys.argv) > 2:
-    sys.exit("Too many command-line arguments")
-elif len(sys.argv) <2:
-    sys.exit("Too few command-line arguments")
+# if len(sys.argv) > 2:
+#     sys.exit("Too many command-line arguments")
+# elif len(sys.argv) <2:
+#     sys.exit("Too few command-line arguments")
 
 try:
-    if sys.argv[1].split(".")[1] != "py":
-        sys.exit("Not a python file")
-    file = open(sys.argv[1],"r")
+    # if sys.argv[1].split(".")[1] != "py":
+    #     sys.exit("Not a python file")
+    file = open("check.py","r")
 except ValueError:
     sys.exit("Not a python file")
 except IndexError:
