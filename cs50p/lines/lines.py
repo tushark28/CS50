91fe0c9430a080701ee983cf5788d2ef163ef68e
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-12 16:04:59 +0000

    Sun, Feb 12, 2023, 9:34 PM +05:30

--- cs50p/lines/lines.py
+++ cs50p/lines/lines.py
@@ -8,7 +8,15 @@ elif len(sys.argv) <2:
 try:
     if sys.argv[1].split(".")[1] != "py":
         sys.exit("Not a python file")
+    file = open(sys.argv[1],"r")
 except ValueError:
     sys.exit("Not a python file")
 except IndexError:
     sys.exit("Not a python file")
+except FileNotFoundError:
+    sys.exit("File does not Exist")
+
+for line in file:
+    if line[]
+
+file.close()
