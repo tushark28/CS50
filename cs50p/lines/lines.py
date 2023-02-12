68869eec90df0d5396057ba605ad7e51e7f52adf
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-12 17:03:45 +0000

    Sun, Feb 12, 2023, 10:33 PM +05:30

--- cs50p/lines/lines.py
+++ cs50p/lines/lines.py
@@ -1,14 +1,14 @@
 import sys
 
-# if len(sys.argv) > 2:
-#     sys.exit("Too many command-line arguments")
-# elif len(sys.argv) <2:
-#     sys.exit("Too few command-line arguments")
+if len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+elif len(sys.argv) <2:
+    sys.exit("Too few command-line arguments")
 
 try:
-    # if sys.argv[1].split(".")[1] != "py":
-    #     sys.exit("Not a python file")
-    file = open("check.py","r")
+    if sys.argv[1].split(".")[1] != "py":
+        sys.exit("Not a python file")
+    file = open(sys.argv[1],"r")
 except ValueError:
     sys.exit("Not a python file")
 except IndexError:
@@ -18,9 +18,10 @@ except FileNotFoundError:
 
 count = 0
 for line in file:
+    print(f"{len(line)} and \n {len(line)*'.'}")
     if line[0] == '#':
         continue
-    elif len(line) == len(line)*" ":
+    elif line == len(line)*" ":
         continue
     else:
         count+=1
