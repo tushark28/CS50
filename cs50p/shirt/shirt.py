afc1e810bf951108a37e1d606fa8b29446fea219
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-13 15:03:11 +0000

    Mon, Feb 13, 2023, 8:33 PM +05:30

---
+++ cs50p/shirt/shirt.py
@@ -0,0 +1,17 @@
+import sys
+
+if len(sys.argv) > 3:
+    sys.exit("Too many command-line arguments")
+elif len(sys.argv) <3:
+    sys.exit("Too few command-line arguments")
+
+try:
+    if sys.argv[1].split(".")[1] != "csv":
+        sys.exit("Not a csv file")
+    file = open(sys.argv[1],"r")
+except ValueError:
+    sys.exit("Not a csv file")
+except IndexError:
+    sys.exit("Not a csv file")
+except FileNotFoundError:
+    sys.exit("File does not Exist")
