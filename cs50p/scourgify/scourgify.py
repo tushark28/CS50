f07813e4a82168cee61eaeac814c5e4df0815b42
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-13 15:02:14 +0000

    Mon, Feb 13, 2023, 8:32 PM +05:30

--- cs50p/scourgify/scourgify.py
+++ cs50p/scourgify/scourgify.py
@@ -27,6 +27,6 @@ file.close()
 
 with open(sys.argv[2],"w") as file:
     writer = csv.DictWriter(file,fieldnames=["first","last","house"])
-    writer.writeheader()
+    writer.writeheader(0)
     for line in new:
         writer.writerow({"first" : line["first"], "last":line["last"],"house":line["house"]})
