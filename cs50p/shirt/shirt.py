365248f2240cbd592967b003c9707e82af10d2f0
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-13 15:08:34 +0000

    Mon, Feb 13, 2023, 8:38 PM +05:30

--- cs50p/shirt/shirt.py
+++ cs50p/shirt/shirt.py
@@ -6,8 +6,10 @@ elif len(sys.argv) <3:
     sys.exit("Too few command-line arguments")
 
 try:
-    if sys.argv[1].split(".")[1] != "csv":
-        sys.exit("Not a csv file")
+    if ".jpg" not in sys.argv[1].lower() and ".jpeg" not in sys.argv[1].lower() and ".png" not in sys.argv[1].lower():
+        sys.exit("Invalid Input")
+    if sys.argv[1].split(".")[1] != sys.argv[2].split(".")[1]:
+        sys.exit()
     file = open(sys.argv[1],"r")
 except ValueError:
     sys.exit("Not a csv file")
