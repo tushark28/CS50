76abd2c974db8be961b3011acd3dd909f051c8ec
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-13 11:04:46 +0000

    Mon, Feb 13, 2023, 4:34 PM +05:30

---
+++ cs50p/pizza/pizza.py
@@ -0,0 +1,17 @@
+import sys
+
+if len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+elif len(sys.argv) <2:
+    sys.exit("Too few command-line arguments")
+
+try:
+    if sys.argv[1].split(".")[1] != "py":
+        sys.exit("Not a python file")
+    file = open(sys.argv[1],"r")
+except ValueError:
+    sys.exit("Not a python file")
+except IndexError:
+    sys.exit("Not a python file")
+except FileNotFoundError:
+    sys.exit("File does not Exist")
