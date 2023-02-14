178086f9a2ce5df4ad6d293d71f05b07005af0bd
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-14 18:14:31 +0000

    Tue, Feb 14, 2023, 11:44 PM +05:30

--- cs50p/watch/watch.py
+++ cs50p/watch/watch.py
@@ -9,7 +9,8 @@ def parse(s):
     if match := re.search(r"<iframe (?:width=\"[0-9]+\" )?(?:eight=\"[0-9]+\" )?src=\"https?://(?:www\.)?youtube\.com/embed/(.)\" (?:title=\".\" )?(?:frameborder=\"[0-9]+\" )?(?:allow=\"(?:accelerometer; )?(?:autoplay; )?(?:clipboard-write; )?(?:encrypted-media; )?(?:gyroscope; )(?:picture-in-picture)?\" )?(?:allowfullscreen)?></iframe>",s):
         print(match.groups(1))
         return True
-
+    else:
+        return False
 
 if __name__ == "__main__":
     main()
