a2599d716d44ae0a5fe36bf800051ef1a3e087a0
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-15 10:55:02 +0000

    Wed, Feb 15, 2023, 4:25 PM +05:30

--- cs50p/working.py
+++ cs50p/working.py
@@ -8,7 +8,7 @@ def main():
 def convert(s):
     if matches := re.search(r"([0-9]+)(:[0-9]+)? (AM|PM) to ([0-9]+)(:[0-9]+)? (AM|PM)",s):
         print(matches.groups())
-        print(matches.group(1))
+        if not 1<= int(matches.group(1)) <= 12
 
 
 
