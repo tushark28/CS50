194e54cdb2beb9d63b6561bde271f4ed36a22ada
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-15 11:04:05 +0000

    Wed, Feb 15, 2023, 4:34 PM +05:30

--- cs50p/working.py
+++ cs50p/working.py
@@ -8,7 +8,25 @@ def main():
 def convert(s):
     if matches := re.search(r"([0-9]+)(:[0-9]+)? (AM|PM) to ([0-9]+)(:[0-9]+)? (AM|PM)",s):
         print(matches.groups())
-        if not 1<= int(matches.group(1)) <= 12
+        if not 1<= int(matches.group(1)) <= 12:
+            raise ValueError
+
+        if matches.group(2) != None:
+            if not 0<=int(matches.group(2))<=59:
+                raise ValueError
+
+        if matches.group(3) != "AM" or matches.group(3) != "PM":
+            raise ValueError
+
+        if not 1<= int(matches.group(4)) <= 12:
+            raise ValueError
+
+        if matches.group(5) != None:
+            if not 0<=int(matches.group(5))<=59:
+                raise ValueError
+
+        if matches.group(6) != "AM" or matches.group(6) != "PM":
+            raise ValueError
 
 
 
