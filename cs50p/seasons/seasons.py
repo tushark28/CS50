94eeaa6f6cccae2d46d11fb8bb7d9a99d21a8e2f
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-22 14:19:22 +0000

    Wed, Feb 22, 2023, 7:49 PM +05:30

--- cs50p/seasons/seasons.py
+++ cs50p/seasons/seasons.py
@@ -2,8 +2,8 @@ from datetime import date
 import sys
 
 def main():
-    birth = input("Date of Birth: ")
-    
+    # birth = input("Date of Birth: ")
+    print(date.today())
 
 
 if __name__ == "__main__":
