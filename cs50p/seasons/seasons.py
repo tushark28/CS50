b6939868f7c9fd4d0bd8d705bf08c50794e384a8
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-22 14:23:46 +0000

    Wed, Feb 22, 2023, 7:53 PM +05:30

--- cs50p/seasons/seasons.py
+++ cs50p/seasons/seasons.py
@@ -4,7 +4,7 @@ import sys
 def main():
     # birth = input("Date of Birth: ")
     print(date.today())
-    print(date.fromtimestamp("2001-06-28"))
+    print(date.fromtimestamp(date.fromisoformat('2001-06-28')))
 
 if __name__ == "__main__":
     main()
