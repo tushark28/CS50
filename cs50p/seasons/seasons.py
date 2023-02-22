1cedf2c185719c839440602c604a9a8dbe032e8c
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-22 14:39:42 +0000

    Wed, Feb 22, 2023, 8:09 PM +05:30

--- cs50p/seasons/seasons.py
+++ cs50p/seasons/seasons.py
@@ -3,10 +3,13 @@ import sys
 
 def main():
     # birth = input("Date of Birth: ")
-    print(datetime.date.today())
+    print(datetime.datetime.today())
     # year,month,day = birth.split("-")
     try:
         print(datetime.datetime(2001,6,1))
+    except:
+        pass
+
 
 if __name__ == "__main__":
     main()
