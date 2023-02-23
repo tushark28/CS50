40abc7cc5a24df34d177d340329356be53193cee
Tushar Kamboj <iamtktushar@gmail.com>
2023-02-23 17:47:53 +0000

    Thu, Feb 23, 2023, 11:17 PM +05:30

--- cs50p/jar/test_jar.py
+++ cs50p/jar/test_jar.py
@@ -1,3 +1,12 @@
 from jar import Jar
 
 def test_init():
+
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
